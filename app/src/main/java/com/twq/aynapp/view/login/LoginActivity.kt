0f07eb252f7294d415f0d9201a7d839c9f5a6573
AliBoholaiqa <Aliboholaiqa@gmail.com>
2021-12-12 20:12:44 +0300

    Login and register using firebase successfully

--- app/src/main/java/com/twq/aynapp/view/login/LoginActivity.kt
+++ app/src/main/java/com/twq/aynapp/view/login/LoginActivity.kt
@@ -1,24 +1,51 @@
 package com.twq.aynapp.view.login
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.twq.aynapp.R
 import com.twq.aynapp.databinding.ActivityLoginBinding
 import com.twq.aynapp.view.home.HomeActivity
 import com.twq.aynapp.view.registration.RegistrationActivity
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
+        val currentUser = auth.currentUser
+
+
         binding.buttonLogin.setOnClickListener {
-            val intent = Intent(this,HomeActivity::class.java)
-            startActivity(intent)
-        }
+                auth.signInWithEmailAndPassword(binding.editTextEmailLogin.text.toString()
+                    ,binding.editTextPasswordLogin.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            val user = auth.currentUser
+                            Log.d(TAG,"User is $user")
+                            Log.d(TAG, "signIn:success")
+                            val intent = Intent(this,HomeActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signIn:failure", task.exception)
+                            Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
         binding.buttonRegistration.setOnClickListener {
             val intent = Intent(this,RegistrationActivity::class.java)
             startActivity(intent)
@@ -27,5 +54,8 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this,HomeActivity::class.java)
             startActivity(intent)
         }
+
+        // Firebase.auth.signOut()
+
     }
 }
--- app/src/main/java/com/twq/aynapp/view/registration/RegistrationActivity.kt
+++ app/src/main/java/com/twq/aynapp/view/registration/RegistrationActivity.kt
@@ -1,11 +1,18 @@
 package com.twq.aynapp.view.registration
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.viewModels
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.twq.aynapp.R
 import com.twq.aynapp.databinding.ActivityRegistrationBinding
 import com.twq.aynapp.view.home.HomeActivity
@@ -13,20 +20,40 @@ import com.twq.aynapp.view.home.HomeFragment
 import com.twq.aynapp.view.login.LoginActivity
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+    private lateinit var db: Firebase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val vm: RegistrationViewModel by viewModels()
+        auth = Firebase.auth
+        val db = Firebase.firestore
+
         binding.buttonRegistrationRegister.setOnClickListener {
-            vm.register(binding.editTextNameRegister.text.toString(),
-                binding.editTextEmailRegister.text.toString(),
-                binding.editTextPasswordRegister.text.toString()).observe(this, {
-                if (it) {
-                    startActivity(Intent(this, HomeActivity::class.java))
+            auth.createUserWithEmailAndPassword(
+            binding.editTextEmailRegister.text.toString(),
+            binding.editTextPasswordRegister.text.toString())
+                .addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val user = hashMapOf(
+                            "username" to binding.editTextNameRegister,
+                            "email" to binding.editTextEmailRegister,
+                            "password" to binding.editTextPasswordRegister
+                        )
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                        db.collection("user").document(auth.currentUser?.uid.toString())
+                            .set(user)
+                    }
+                    else{
+                        Toast.makeText(this, "Unable to sign-in", Toast.LENGTH_SHORT).show()
+                    }
+
+                }.addOnFailureListener { e ->
+                    Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "")
                 }
-            })
         }
 
         binding.buttonLoginRegister.setOnClickListener {
